b7f8cc0ae663737fca7e8c5a4a66d98f591b3e51
cviquez <cviquez@Akureys-MacBook-Pro-2.local>
2019-05-06 19:21:33 -0600

    Header, main content and footer component

---
+++ Angular/AsignacionAngular/RunForestRun/src/app/main-content/main-content.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AgentServiceService} from './agent-service.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.scss']
+})
+export class MainContentComponent implements OnInit {
+    agents$: Object;
+
+  constructor(private agent_service: AgentServiceService) { }
+
+  ngOnInit() {
+    this.agent_service.getAgents().subscribe(
+      data => this.agents$ = data
+    )
+  }
+
+}
